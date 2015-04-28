377ebfcc4659358308560c61a5e29947bae61af2
rita-cribello <rita.roy@mycuratio.com>
2015-04-28 17:22:16 +0530

    Added new files

---
+++ Shabdo-khoj/Shabdo-khoj/ImageFile.h
@@ -0,0 +1,13 @@
+//
+//  ImageFile.h
+//  Shabdo-khoj
+//
+//  Created by technicise on 4/28/15.
+//  Copyright (c) 2015 technicise.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageFile : UIViewController
+
+@end
