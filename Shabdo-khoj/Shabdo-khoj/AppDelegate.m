a10c0608b8bae76e51e41a031f8b3cff116ea9fd
rita-cribello <rita.roy@mycuratio.com>
2015-04-28 15:16:32 +0530

    Added

--- Shabdo-khoj/Shabdo-khoj/AppDelegate.m
+++ Shabdo-khoj/Shabdo-khoj/AppDelegate.m
@@ -17,6 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//    self.ViewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+////    _viewController2.user_id = user_id1;
+////    _viewController2.status = @"other";
+//    self.window.rootViewController = self.ViewController;
+//    [self.window makeKeyAndVisible];
     return YES;
 }
 
